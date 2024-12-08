34016c4751798ec66aaa5cee0d806908e9777806
Oat Teeraphat <oat.teeraphata@gmail.com>
2024-12-08 23:48:00 +0700

    init project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'ubnqc6rprc0c7v',
+    password: 'p2a0e62eb6ce59cbbc78ed9c3442f9c3e65a37aaf669502a98580ae0564b1365f',
+    host: 'c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    port: 5432, // default Postgres port
+    database: 'd3me0rd12s6r07'
+  });
+
+// create TODO LIST
+
+//-- id|task|employee_id|
+
+const createTableTask = () => {
+    pool.query('CREATE TABLE task ( id INT, task VARCHAR(255), employee_id INT );')
+}
+
+const insertTaskTodoList = (id, task, employee_id) => {
+    return pool.query('')
+}
+
+//http://localhost:3000/?id=1&task=ทดสอบ&emp_id=001
+
+app.get('/insertTask', (req, res) => {
+    console.log(req.query)
+    getAllTodoList()
+    res.send({'status' : 'success'})
+});
+
+app.get('/createTable', (req, res) => {
+    createTableTask()
+    res.send({'status' : 'success'})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening at port: ${port}`);
+});
+    
+    
