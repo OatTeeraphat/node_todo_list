2a9dc778386bd802977b068817b49453d399cd36
jamemech <jamemech01@gmail.com>
2024-12-12 06:17:04 +0700

    create task table

--- app.js
+++ app.js
@@ -8,23 +8,26 @@ const pool = new Pool({
     password: 'p2a0e62eb6ce59cbbc78ed9c3442f9c3e65a37aaf669502a98580ae0564b1365f',
     host: 'c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
     port: 5432, // default Postgres port
-    database: 'd3me0rd12s6r07'
+    database: 'd3me0rd12s6r07',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // create TODO LIST
 
-//-- id|task|employee_id|
+//-- id|task|emp_id|
 
-const createTableTask = () => {
-    pool.query('CREATE TABLE task ( id INT, task VARCHAR(255), employee_id INT );')
+const createTaskTable = () => {
+    pool.query('CREATE TABLE task (id INT, task VARCHAR(255), emp_id VARCHAR(255))')
 }
 
-const insertTaskTodoList = (id, task, employee_id) => {
-    pool.query('INSERT INTO task (id, task, employee_id) VALUES (?, ?, ?)', [id, task, employee_id]);
+const insertTaskTodoList = (id, task, emp_id) => {
+    pool.query('INSERT INTO task (id, task, emp_id) VALUES ($1, $2, $3)', [id, task, emp_id]);
 }
 
 app.get('/createTable', (req, res) => {
-    createTableTask()
+    createTaskTable()
     res.send({ 'status': 'success' })
 })
 
@@ -32,12 +35,17 @@ app.get('/createTable', (req, res) => {
 
 app.get('/insertTaskTodoList', (req, res) => {
     console.log(req.query)
-    insertTaskTodoList(req.query)
+
+    const id = Number(req.query.id)
+    const task = req.query.task
+    const emp_id = req.query.emp_id
+
+    insertTaskTodoList(id, task, emp_id)
     res.send({ 'status': 'success' })
 });
+
 
 
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`);
 });
-
